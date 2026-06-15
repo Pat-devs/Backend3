cd429339fb33e97a708dbb0c2d1704bbc9d51ef2
Pat-devs <95124571+Pat-devs@users.noreply.github.com>
2026-06-15 10:05:33 +0200

    Overload local methods. This gives error due to C# not supporting overloading of local methods in the main program.cs. Fix in next commit.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿// method overloads
+
+// overloads in built in methods
+//Console.WriteLine("Hi"); // WriteLine can take a string (this is one overload)
+
+//Console.WriteLine(1234); // another overload
+
+//Console.WriteLine('A'); // another overload 
+
+
+static void Greet(string data)
+{
+    Console.WriteLine(data);
+}
+
+static void Greet(int data)
+{
+    Console.WriteLine(data);
+}
+
+
+Greet("hi there");
+
+Greet(1234);
